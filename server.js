660d15c1778c4cd3e0e421fdf4d2fca7a83644c2
Luis Ruben Gonzalez Pagan <152279868+Luisru42@users.noreply.github.com>
2025-03-31 14:34:07 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors'); // Import cors
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Use cors middleware first
+app.use(cors());
 
 // Use Express's built-in body parsing middleware
 app.use(express.urlencoded({ extended: true }));
@@ -19,5 +23,3 @@ app.post('/send-email', (req, res) => {
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
-const cors = require('cors');
-app.use(cors());
