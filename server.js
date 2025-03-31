a01323049712d9664ebeccd0d1153abd82a5a188
Luis Ruben Gonzalez Pagan <152279868+Luisru42@users.noreply.github.com>
2025-03-31 14:28:35 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from your Node.js server!');
+// Use Express's built-in body parsing middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/send-email', (req, res) => {
+    const { name, email, message } = req.body;
+    // Process the email sending logic here
+    console.log("received form submission");
+    console.log(name, email, message);
+
+    //send response
+    res.send('Email sent successfully!'); // Or handle errors
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
+const cors = require('cors');
+app.use(cors());
